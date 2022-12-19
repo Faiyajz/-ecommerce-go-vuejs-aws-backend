c11f75d2a1f9f0b774144f1372fef5ed40e3563f
faiyaj <faiyajz007@gmail.com>
2022-12-19 07:30:06 +0600

    product server fixed

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,12 +1,15 @@
 package server
 
 import (
-	"backend/internal/category"
-	"backend/internal/product"
 	"fmt"
 	"net/http"
 
+	"backend/internal/product"
+
 	"github.com/Rhymond/go-money"
+
+	"backend/internal/category"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -21,63 +24,60 @@ type Config struct {
 
 func New(config Config) (*Server, error) {
 	engine := gin.Default()
-
 	server := &Server{
 		engine: engine,
 		port:   config.Port,
 	}
-
-	engine.GET("/products", server.Products)
 	engine.GET("/categories", server.Categories)
-
+	engine.GET("/products", server.Products)
 	return server, nil
 }
 
 func (server *Server) Run() error {
-	return server.engine.Run(fmt.Sprintf("%d", server.port))
-}
-
-func (server *Server) Products(ctx *gin.Context) {
-	products := []product.Product{
-		{
-			ID:               "868",
-			Name:             "Handbook",
-			Description:      "July, 2022",
-			PriceVATExcluded: money.New(1000, "USD"),
-			VAT:              money.New(1200, "USD"),
-		},
-		{
-			ID:               "869",
-			Name:             "Handbook Hardcover",
-			Description:      "December, 2022",
-			PriceVATExcluded: money.New(2000, "USD"),
-			VAT:              money.New(2500, "USD"),
-		},
-		{
-			ID:               "870",
-			Name:             "Handbook",
-			Description:      "New Edition",
-			PriceVATExcluded: money.New(1500, "USD"),
-			VAT:              money.New(1800, "USD"),
-		},
-	}
-	ctx.Header("Access-Control-Allow-Origin", "http://localhost:8080")
-	ctx.JSON(200, products)
+	return server.engine.Run(fmt.Sprintf(":%d", server.port))
 }
 
 func (server *Server) Categories(ctx *gin.Context) {
 	categories := []category.Category{
 		{
-			ID:          "1",
-			Name:        "HardCover",
-			Description: "A Hard Copy of the Handbook",
+			ID:          "42",
+			Name:        "Plushies",
+			Description: "kdsjdjsidjisdj",
 		},
 		{
-			ID:          "2",
-			Name:        "Digital Version",
-			Description: "A Soft Copy of the Handbook",
+			ID:          "43",
+			Name:        "T-Shirts",
+			Description: "kdsjdjsidjisdj",
 		},
 	}
 	ctx.Header("Access-Control-Allow-Origin", "http://localhost:8080")
 	ctx.JSON(http.StatusOK, categories)
+}
+
+func (server *Server) Products(ctx *gin.Context) {
+	products := []product.Product{
+		{
+			ID:               "42",
+			Name:             "Test",
+			Description:      "This is my product",
+			PriceVATExcluded: money.New(100, "EUR"),
+			VAT:              money.New(200, "EUR"),
+		},
+		{
+			ID:               "43",
+			Name:             "Test",
+			Description:      "This is my product",
+			PriceVATExcluded: money.New(100, "EUR"),
+			VAT:              money.New(250, "EUR"),
+		},
+		{
+			ID:               "44",
+			Name:             "Test",
+			Description:      "This is my product",
+			PriceVATExcluded: money.New(189, "EUR"),
+			VAT:              money.New(200, "EUR"),
+		},
+	}
+	ctx.Header("Access-Control-Allow-Origin", "http://localhost:8080")
+	ctx.JSON(200, products)
 }
